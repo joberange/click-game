566e161a7fae766c308995e458e0e4134f40f3af
Jobe Range <rangejobe@gmail.com>
2018-11-03 11:48:34 -0700

    Updates

---
+++ precache-manifest.8be2f67317ffb332dbbaead7e5939db6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/click-game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "451cb8796073a9e8640b",
+    "url": "/click-game/static/js/runtime~main.451cb879.js"
+  },
+  {
+    "revision": "816b4d73b831701fa04c",
+    "url": "/click-game/static/js/main.816b4d73.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/click-game/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "816b4d73b831701fa04c",
+    "url": "/click-game/static/css/main.9faced0a.chunk.css"
+  },
+  {
+    "revision": "31c801b0e061aed2f78df9ca4d019624",
+    "url": "/click-game/index.html"
+  }
+];
